fac51fe9a28f857c219d1f600235b38d5a27c82b
MPodbelsky <147852369gggG>
2018-09-25 20:07:50 +0300

    Tests Sobe and DenProg 1251

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import com.beust.jcommander.Parameter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
